3a14a42360062214c7b37383bf4abde3606317a6
Tad <guotai@live.com>
2017-11-26 20:32:34 +0800

    Update bbr.sh

--- bbr.sh
+++ bbr.sh
@@ -1,82 +1,112 @@
-#!/bin/bash
+#!/usr/bin/env bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
 
-CentOS_Version=`cat /etc/redhat-release | grep -oE '[0-9]+\.[0-9]+' | cut -d'.' -f1`
-KVersion=4.9.0-1
+#Check Root
+[ $(id -u) != "0" ] && { echo "Error: You must be root to run this script"; exit 1; }
 
-if [ -z ${CentOS_Version} ]
-then
-	CentOS_Version=0
+#Check OS
+if [[ -f /etc/redhat-release ]]; then
+		release="centos"
+	elif cat /etc/issue | grep -q -E -i "debian"; then
+		release="debian"
+	elif cat /etc/issue | grep -q -E -i "ubuntu"; then
+		release="ubuntu"
+	elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
+		release="centos"
+	elif cat /proc/version | grep -q -E -i "debian"; then
+		release="debian"
+	elif cat /proc/version | grep -q -E -i "ubuntu"; then
+		release="ubuntu"
+	elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
+		release="centos"
 fi
+bit=`uname -m`
+dir=`pwd`
+installbbr(){
+	#Install GCC
+	apt-get update
+	apt-get install build-essential -y
+	apt-get install make gcc-4.9 -y
 
-if [ ${CentOS_Version} -lt 6 ]
-then
-	echo "Sorry, I can only support CentOS 6/7 yet."
-	exit
-fi
 
-if [[ `getconf WORD_BIT` = '32' && `getconf LONG_BIT` = '64' ]] ; then
-	BIT_VER=x64
-else
-	BIT_VER=x86
-fi
+	#Download Kernel V4.10
+	wget -O headers-all.deb http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.10.15/linux-headers-4.10.15-041015_4.10.15-041015.201705080411_all.deb
+	dpkg -i headers-all.deb
 
-if [ ${BIT_VER} != 'x64' ]
-then
-	echo "Sorry, I can only support x64 yet."
-	exit
-fi
-
-echo "Now I will replace the system kernel to ${KVersion}..."
-echo "Start installing"
-
-MODSCRIPT=/usr/share/dracut/modules.d/90kernel-modules/installkernel
-QOCC=`grep blk_init_queue $MODSCRIPT|wc -l`
-if [ $QOCC -eq 1 ]
-then
-	sed -i 's/blk_init_queue/blk_mq_init_queue/' $MODSCRIPT
-fi
-if [ ${CentOS_Version} -eq 7 ]
-then
-	rpm -Uvh --force http://soft.wellphp.com/kernels/x86_64/kernel-ml-${KVersion}.el${CentOS_Version}.centos.x86_64.rpm
-else
-	rpm -Uvh --force http://soft.wellphp.com/kernels/x86_64/kernel-ml-${KVersion}.el${CentOS_Version}.x86_64.rpm
-fi
-if [ $QOCC -eq 1 ]
-then
-	sed -i 's/blk_mq_init_queue/blk_init_queue/' $MODSCRIPT
-fi
-echo "Checking if the installtion is ok"
-KGRUB2=`ls /etc/grub2.cfg|wc -l`
-if [ ${KGRUB2} -eq 1 ]
-then
-	INS_OK=`awk -F\' '$1=="menuentry " {print i++ " : " $2}' /etc/grub2.cfg | grep ${KVersion} | grep -i -v debug | grep -i -v rescue | cut -d' ' -f1`
-	if [ -z ${INS_OK} ]
-	then
-		echo "Sorry, install failed, please contact the author"
-		exit
+	if [[ ${bit} == "i386" ]]; then
+		wget --no-check-certificate -O headers.deb http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.10.15/linux-headers-4.10.15-041015-generic_4.10.15-041015.201705080411_i386.deb
+		wget --no-check-certificate -O image.deb http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.10.15/linux-image-4.10.15-041015-generic_4.10.15-041015.201705080411_i386.deb
+	elif [[ ${bit} == "x86_64" ]]; then
+		wget --no-check-certificate -O headers.deb http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.10.15/linux-headers-4.10.15-041015-generic_4.10.15-041015.201705080411_amd64.deb
+		wget --no-check-certificate -O image.deb http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.10.15/linux-image-4.10.15-041015-generic_4.10.15-041015.201705080411_amd64.deb
+	else
+			echo -e "不支持 ${bit} !" && exit 1
 	fi
-	yum install -y grub2-tools
-	
-	grub2-set-default ${INS_OK}
-else
-	KGRUB=`ls /boot/grub/grub.conf|wc -l`
-	if [ ${KGRUB} -eq 1 ]
-	then
-		INS_OK=`grep '^title ' /boot/grub/grub.conf | awk -F'title ' '{print i++ " : " $2}' | grep ${KVersion} | grep -i -v debug | grep -i -v rescue | cut -d' ' -f1`
-		if [ -z ${INS_OK} ]
-		then
-			echo "Sorry, install failed, please contact the author"
-			exit
+
+	dpkg -i headers.deb
+	dpkg -i image.deb
+	rm -rf headers-all.deb
+	rm -rf headers.deb image.deb
+
+	#Uninstall Other Kernel
+	deb_total=`dpkg -l | grep linux-image | awk '{print $2}' | grep -v "4.10.15" | wc -l`
+	if [ "${deb_total}" > "1" ]; then
+		echo -e "检测到 ${deb_total} 个其余内核，开始卸载..."
+		for((integer = 1; integer <= ${deb_total}; integer++))
+		do
+			deb_del=`dpkg -l|grep linux-image | awk '{print $2}' | grep -v "4.10.15" | head -${integer}`
+			echo -e "开始卸载 ${deb_del} 内核..."
+			apt-get purge -y ${deb_del}
+			echo -e "卸载 ${deb_del} 内核卸载完成，继续..."
+		done
+		deb_total=`dpkg -l|grep linux-image | awk '{print $2}' | grep -v "4.10.15" | wc -l`
+		if [ "${deb_total}" = "0" ]; then
+			echo -e "内核卸载完毕，继续..."
+		else
+			echo -e " 内核卸载异常，请检查 !" && exit 1
 		fi
-		sed -i "s/^default.*/default=${INS_OK}/" /boot/grub/grub.conf
+	else
+		echo -e " 检测到 内核 数量不正确，请检查 !" && exit 1
 	fi
-fi
 
-echo " "
-echo "Installation is completed, now you can reboot the system. "
-echo "You should check BBR after the rebooting using command: "
-echo " "
-echo "     sysctl -a|grep congestion_control"
+	#Finish Install
+	update-grub
+	echo -e "\033[42;37m[注意]\033[0m 重启VPS后，请重新运行脚本开启魔改BBR \033[42;37m bash bbr.sh start \033[0m"
+	stty erase '^H' && read -p "需要重启VPS后，才能开启BBR，是否现在重启 ? [Y/n] :" yn
+	[ -z "${yn}" ] && yn="y"
+		if [[ $yn == [Yy] ]]; then
+			echo -e "\033[41;37m[信息]\033[0m VPS 重启中..."
+			reboot
+		fi
+}
 
 
+startbbr(){
+    mkdir -p $dir/tsunami && cd $dir/tsunami
+	wget --no-check-certificate -O ./tcp_tsunami.c https://gist.github.com/anonymous/ba338038e799eafbba173215153a7f3a/raw/55ff1e45c97b46f12261e07ca07633a9922ad55d/tcp_tsunami.c
+	echo "obj-m:=tcp_tsunami.o" > Makefile
+	make -C /lib/modules/$(uname -r)/build M=`pwd` modules CC=/usr/bin/gcc-4.9
+	insmod tcp_tsunami.ko
+    	cp -rf ./tcp_tsunami.ko /lib/modules/$(uname -r)/kernel/net/ipv4
+    	depmod -a
+    	modprobe tcp_tsunami
+	rm -rf /etc/sysctl.conf
+	wget -O /etc/sysctl.conf -N --no-check-certificate https://raw.githubusercontent.com/FunctionClub/YankeeBBR/master/sysctl.conf
+	sysctl -p
+    cd .. && rm -rf $dir/tsunami
+	echo "魔改版BBR启动成功！"
+}
 
+
+action=$1
+[ -z $1 ] && action=install
+case "$action" in
+	install|start)
+	${action}bbr
+	;;
+	*)
+	echo "输入错误 !"
+	echo "用法: { install | start }"
+	;;
+esac
